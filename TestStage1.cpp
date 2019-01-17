0bcd553139f0e0cf228a679b1ca5a50e4a671a89
LEE <powerlsj7@gmail.com>
2019-01-18 08:40:25 +0900

    이승재_ 오류된부분 삭제
    오류부분삭제

--- TestStage1.cpp
+++ TestStage1.cpp
@@ -13,8 +13,7 @@ TestStage1::~TestStage1()
 
 HRESULT TestStage1::init()
 {
-	//_pItem = new ITEMBOOM;
-	//_pItem->init(100,100,_pItem->getImage()->GetWidth(),_pItem->getImage()->GetHeight());
+	
 	return S_OK;
 }
 
@@ -28,5 +27,5 @@ void TestStage1::update()
 
 void TestStage1::render()
 {
-	//_pItem->render();
+	
 }
