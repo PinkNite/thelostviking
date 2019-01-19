63a115de85d0c212ebf7d259776da0b8f63cc291
igniteBoom <parkbnmki@gmail.com>
2019-01-19 17:55:45 +0900

    학원 나가기전 커밋
    hange 없수무니다... 죄송합니다...

--- pixelCollision.h
+++ pixelCollision.h
@@ -1,12 +1,19 @@
 #include "player.h"
 #include "map1.h"
+#include "map2.h"
+#include "player.h"
 #pragma once
 
 class pixelCollision
 {
 private:
+	image* _imgMap1Cbg;
+	image* _imgMap2Cbg;
+
+private:
 	PLAYER* _pPlayer;
 	MAP1* _pMap1;
+	MAP2* _pMap2;
 
 public:
 	pixelCollision();
