a00015c3f7669233ebb144d46b8462ff016ece98
mindonghwi <mdh92706@gmail.com>
2019-01-20 20:51:04 +0900

    addVikingClass

---
+++ viking.cpp
@@ -0,0 +1,107 @@
+#include "stdafx.h"
+#include "viking.h"
+
+VIKING::VIKING()
+{
+}
+
+VIKING::~VIKING()
+{
+}
+
+void VIKING::init(int posX, int posY, int width, int height)
+{
+	OBJECT::init(posX, posY, width, height);
+	_nHealthPoint = 3;
+	//좌우
+	_arDirection[static_cast<int>(VIKING::DIRECTION::LEFT)] = "LEFT";
+	_arDirection[static_cast<int>(VIKING::DIRECTION::RIGHT)] = "RIGHT";
+
+	//죽었다 살았다
+	_arLive[static_cast<int>(VIKING::LIFE::ALIVE)] = "ALIVE";
+	_arLive[static_cast<int>(VIKING::LIFE::DEATH)] = "DEATH";
+
+	//아이들 + 행동
+	_vBehavior[static_cast<int>(VIKING::STATE::IDLE)].push_back("NORMAL");
+	_vBehavior[static_cast<int>(VIKING::STATE::IDLE)].push_back("FAT");
+	_vBehavior[static_cast<int>(VIKING::STATE::IDLE)].push_back("MUSIC");
+	_vBehavior[static_cast<int>(VIKING::STATE::IDLE)].push_back("SPECIAL");
+
+	//액션 + 행동
+	_vBehavior[static_cast<int>(VIKING::STATE::ACTION)].push_back("TELEPORT");
+	_vBehavior[static_cast<int>(VIKING::STATE::ACTION)].push_back("RUN");
+	_vBehavior[static_cast<int>(VIKING::STATE::ACTION)].push_back("SKILL_ONE");
+	_vBehavior[static_cast<int>(VIKING::STATE::ACTION)].push_back("FALLDOWN");
+	_vBehavior[static_cast<int>(VIKING::STATE::ACTION)].push_back("SKILL_TWO");
+	_vBehavior[static_cast<int>(VIKING::STATE::ACTION)].push_back("STUN");
+	_vBehavior[static_cast<int>(VIKING::STATE::ACTION)].push_back("PUSH");
+	_vBehavior[static_cast<int>(VIKING::STATE::ACTION)].push_back("ON_LADDER");
+	_vBehavior[static_cast<int>(VIKING::STATE::ACTION)].push_back("ON_LADDER_OVER");
+	_vBehavior[static_cast<int>(VIKING::STATE::ACTION)].push_back("DAMAGE");
+
+	//죽음 + 상태
+	_vBehavior[static_cast<int>(VIKING::STATE::DEATH_MOTION)].push_back("ELECTIRC");
+	_vBehavior[static_cast<int>(VIKING::STATE::DEATH_MOTION)].push_back("FIRE");
+	_vBehavior[static_cast<int>(VIKING::STATE::DEATH_MOTION)].push_back("PRESS");
+	_vBehavior[static_cast<int>(VIKING::STATE::DEATH_MOTION)].push_back("WATER");
+	_vBehavior[static_cast<int>(VIKING::STATE::DEATH_MOTION)].push_back("DROP");
+	_vBehavior[static_cast<int>(VIKING::STATE::DEATH_MOTION)].push_back("SHOP");
+
+	//올라프 방패 방향
+	_arOlafShield[static_cast<const int>(VIKING::OLAFSHIELD::VERTICAL)] = "VERTICAL";
+	_arOlafShield[static_cast<const int>(VIKING::OLAFSHIELD::HORIZENTAL)] = "HORIZENTAL";
+
+	_speed = 300.0f;
+	_maxSpeed = 560.0f;
+	_minSpeed = 300.0f;
+}
+
+
+
+void VIKING::setImage(image * pImage)
+{
+	OBJECT::setImage(pImage);
+}
+
+void VIKING::moveLeft(float acceleration)
+{
+	float fDistance = Mins::presentPowerX(0.0f, _speed) * TIMEMANAGER->getElpasedTime();
+	OBJECT::setPosX(OBJECT::getPosX() - fDistance);
+}
+
+void VIKING::moveRight(float acceleration)
+{
+	float fDistance = -(Mins::presentPowerX(PI, _speed) * TIMEMANAGER->getElpasedTime());
+	OBJECT::setPosX(OBJECT::getPosX() + fDistance);
+}
+
+void VIKING::moveUp(float acceleration)
+{
+	float fDistance = -(Mins::presentPowerY(PI / 2.0f, _speed) * TIMEMANAGER->getElpasedTime());
+	OBJECT::setPosY(OBJECT::getPosY() - fDistance);
+}
+
+void VIKING::moveDown(float acceleration)
+{
+	float fDistance = Mins::presentPowerY(PI2 - PI / 2.0f, _speed) * TIMEMANAGER->getElpasedTime();
+	OBJECT::setPosY(OBJECT::getPosY() + fDistance);
+}
+
+//점프는 에릭에서해야한다!!!!
+void VIKING::skillOne()
+{
+	//D스킬
+	//에릭 -> 해딩
+	//발로그 -> 활
+	//올라프 -> 방패 방향 전환 -> 애니메이션은 나는것으로 한다!!!!
+}
+
+void VIKING::skillTwo()
+{
+	//F스킬
+	//에릭 -> 점프
+	//발로그 -> 검
+	//올라프 -> 방패 방향 전환 -> 애니메이션은 나는 방향 전환으로 한다.
+}
+
+//애니메이션은 체크는 안에서 어떻게든 처리하자!!!!!!!!!!!!!!!!!!
--- viking.h
+++ viking.h
@@ -2,6 +2,143 @@
 #include "object.h"
 #include "stdafx.h"
 
+enum { VIKING_WIDTH = 64 };
+enum { VIKING_HEIGHT = 64 };
+
 class VIKING : public OBJECT {
-	
+public:
+	VIKING();
+	~VIKING();
+
+	enum class DIRECTION {
+		LEFT = 0,
+		RIGHT,
+		MAX
+	};
+
+	//막는 방향
+	enum class OLAFSHIELD
+	{
+		VERTICAL,
+		HORIZENTAL,
+		MAX
+	};
+
+	enum class LIFE {
+		ALIVE = 0,
+		DEATH,
+		MAX
+	};
+
+	enum class STATE
+	{
+		IDLE = 0,
+		ACTION,
+		DEATH_MOTION,
+		MAX
+	};
+
+	enum class IDLE {
+		NORMAL = 0,
+		FAT,
+		MUSIC,
+		SPECIAL,
+		MAX
+	};
+
+	enum class ACTION {
+		TELEPORT = 0,
+		RUN,
+		FALLDOWN,
+		STUN,
+		PUSH,
+		ON_LADDER,
+		ON_LADDER_OVER,
+		DAMEGE,
+		SKILL_ONE,
+		SKILL_TWO,
+		MAX
+	};
+
+	enum class DEATH_MOTION {
+		ELECTRIC = 0,
+		FIRE,
+		PRESS,
+		FALLIN,
+		DROP,
+		SHOT,
+		MAX
+	};
+
+
+protected:
+	//방향 + 살다 + 상태 + 아이들
+	//액션
+	//액션 콜백 아이들
+	//살다 콜백 죽음
+	//액션 콜백 액션
+
+
+	//플레이어의 액션을 꺼내오는 
+	int				_direction; //방향
+	int				_life;	    //살았나 죽었나
+	int				_state;	    //상태
+	int				_behavior;  //행동
+
+	string			_arDirection[static_cast<const int>(VIKING::DIRECTION::MAX)];			//방향
+	string			_arLive[static_cast<const int>(VIKING::LIFE::MAX)];						//살다 죽다
+	string			_arOlafShield[static_cast<const int>(VIKING::OLAFSHIELD::MAX)];			//올라프 방패 막는 방향
+	vector<string>	_vBehavior[3];			//아이들, 액션, 죽음모션
+
+
+	float			_speed;					//속도
+	float			_minSpeed;				//최저속도
+	float			_maxSpeed;				//최대속도
+
+	float			_moveSpeed;				//진짜 움직이는 속도값을 준다
+
+	int				_nHealthPoint;			//체력
+
+	animation*		_pAnimation;				//실행할 애니메이션을 담는다
+public:
+	void init(int posX, int posY, int width, int height);
+	virtual void update()			abstract;
+	virtual void release()			abstract;
+	virtual void render(HDC hdc)	abstract;
+
+	void setImage(image* pImage);
+
+public:
+	//get set 모음
+	inline float getPosX() { return _posX; }
+	inline float getPosY() { return _posY; }
+	inline int getWidth() { return _width; }
+	inline int getHeight() { return _height; }
+	inline int getLeft() { return _left; }
+	inline int getTop() { return _top; }
+	inline image* getIamge() { return _pImg; }
+
+
+	inline void setPosX(float posX) { _posX = posX; }
+	inline void setPosY(float posY) { _posY = posY; }
+	inline void setWidth(int width) { _width = width; }
+	inline void setHeight(int height) { _height = height; }
+
+	//이동함수
+	//움직임 애니메이션 변경 생각
+	//현 상태를 본다.
+	//아이들이면 달리는 달리는 액션으로 전환
+	//액션이면 (에릭 스킬1 -> 점프, 올라프 스킬1,전부 떨어지고 있거나) 방향만 맞게 전환한다.
+	//올라프는 스킬1 상태면 스킬2 호출후 스킬2는 다하고 스킬 1 콜백해버리자
+	virtual void moveLeft(float acceleration);
+	virtual void moveRight(float acceleration);
+	virtual void moveUp(float acceleration);
+	virtual void moveDown(float acceleration);
+
+
+
+	//스킬 각자 받아서 공격이나 특이 행동을 쓸때 쓰는 오버라이딩 함수
+	virtual void skillOne();
+	virtual void skillTwo();
+
 };
