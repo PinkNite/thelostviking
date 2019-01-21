a6db5401307e313b279d7a32503c3cef3e2645a9
igniteBoom <parkbnmki@gmail.com>
2019-01-21 12:32:01 +0900

    map2.bmp 업데이트
    업데이트

--- pixelCollision.cpp
+++ pixelCollision.cpp
@@ -29,7 +29,7 @@ void pixelCollision::update()
 	for (int i = _probeY - 10; i <= _probeY; ++i)
 	{
 		COLORREF color = GetPixel(IMAGEMANAGER->findImage("map2Collision")->getMemDC(),
-			_pPlayer->getPosX(), i);
+			_pPlayer->getPosX() - _pPlayer->getWidth() / 2 + 12, i);
 
 		int r = GetRValue(color);
 		int g = GetGValue(color);
@@ -46,10 +46,31 @@ void pixelCollision::update()
 			_isCollision = false;
 		}
 	}
+
+	for (int i = _probeY - 10; i <= _probeY; ++i)
+	{
+		COLORREF color = GetPixel(IMAGEMANAGER->findImage("map2Collision")->getMemDC(),
+			_pPlayer->getPosX() + _pPlayer->getWidth() / 2 - 12, i);
+
+		int r = GetRValue(color);
+		int g = GetGValue(color);
+		int b = GetBValue(color);
+
+		if ((r == 255 && g == 0 && b == 255))
+		{
+			_pPlayer->setPosY(i - _pPlayer->getHeight() / 2);
+			_isCollision = true;
+			break;
+		}
+		else
+		{
+			_isCollision = false;
+		}
+	}
 	printf("Ãæµ¹ : %d\n", _isCollision);
 }
 
 void pixelCollision::render(HDC hdc)
 {
-	_imgMap2Cbg->render(hdc);
+	//_imgMap2Cbg->render(hdc);
 }
